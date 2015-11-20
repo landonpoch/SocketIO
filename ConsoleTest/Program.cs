1f2a4db687a53dcb03e55fe083741ff176b8d39e
Landon Poch <landon.poch@echostar.com>
2015-11-20 01:26:20 -0700

    Work towards generic callbacks.

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,57 @@
+﻿using SocketIO;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                new Program().Run();
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e);
+            }
+        }
+
+        private void Run()
+        {
+            using (var client = new Client())
+            {
+                client.Connect("http://localhost:3000");
+                using (Socket socket = client.GetSocket())
+                {
+                    RunAsyncTest(tcs =>
+                    {
+                        socket.On("test2", () => Console.WriteLine("Action Called!"));
+                        socket.On<string>("test3", response => tcs.TrySetResult(null));
+                        socket.Emit("test");
+                    })
+                    .Wait();
+                }
+            }    
+        }
+
+        private async Task RunAsyncTest(Action<TaskCompletionSource<object>> action)
+        {
+            var tcs = new TaskCompletionSource<object>();
+            try
+            {
+                action(tcs);
+            }
+            catch (Exception e)
+            {
+                tcs.TrySetResult(null);
+            }
+            await tcs.Task;
+        }
+    }
+}
--- SocketIO.Test/UnitTest1.cs
+++ SocketIO.Test/UnitTest1.cs
@@ -1,32 +1,50 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace SocketIO.Test
 {
     [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
-        public void TestMethod1()
+    public class UnitTest1 : TestBase
+    {   
+        private void TestMethod1(TaskCompletionSource<object> tcs)
         {
             using (var client = new Client())
             {
-                client.Connect("");
+                client.Connect("http://localhost:3000");
                 using (Socket socket = client.GetSocket())
                 {
-                    socket.On("", () =>
-                    {
+                    socket.On("test2", () => Console.WriteLine("Action Called!"));
 
-                    });
+                    socket.On<string>("", response => tcs.TrySetResult(null));
 
-                    socket.On<UnitTest1>("", item =>
-                    {
-
-                    });
-
-                    socket.Emit("");
+                    socket.Emit("test");
                 }
             }
+        }
+
+        #region TestMethod Entry Points
+
+        [TestMethod] public async Task TestMethod1() { await RunAsyncTest(TestMethod1); }
+
+        #endregion
+    }
+
+    public abstract class TestBase
+    {
+        protected async Task RunAsyncTest(Action<TaskCompletionSource<object>> action)
+        {
+            var tcs = new TaskCompletionSource<object>();
+            try
+            {
+                action(tcs);
+            }
+            catch (Exception e)
+            {
+                tcs.TrySetResult(null);
+            }
+            await tcs.Task;
         }
     }
 }
